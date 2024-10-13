854dbad1591bb5409792f464a1c696fb4200fe23
Adrian Pacheco <adran9505@gmail.com>
2024-10-13 11:45:47 -0500

    Primera versión del proyecto

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel-mobile').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
